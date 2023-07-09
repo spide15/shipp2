f994ecddf998ad33c90663d0bd9d42b5474bbe26
spide15 <gadadeep4@gmail.com>
2023-07-09 20:08:15 +0530

    undo1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import Nav from './nav'
 import Rout from './rout';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import Footer from './footer';
 import Productdetail from './productdetail';
 const App = () => {
@@ -12,28 +12,31 @@ const App = () => {
   const [detail, setDetail] = useState([])
   //filter product
   const [product, setProduct] = useState(Productdetail)
-  const searchbtn = (product) => {
-    const change = Productdetail.filter((x) => {
+  const searchbtn = (product) => 
+  {
+    const change = Productdetail.filter((x) => 
+    {
       return x.Cat === product
     })
     setProduct(change)
   }
   //product detail
-  const view = (product) => {
-    setDetail([{ ...product }])
+  const view = (product) => 
+  {
+    setDetail([{...product}])
     setClose(true)
   }
 
   // add to cart
-
+  
   console.log(cart)
   return (
     <>
-      <BrowserRouter>
-        <Nav searchbtn={searchbtn} />
-        <Route path='/' element={<Home detail={detail} view={view} close={close} setClose={setClose} addtocart={addtocart} />} />
-       <Footer />
-      </BrowserRouter>
+    <BrowserRouter>
+    <Nav searchbtn={searchbtn}/>
+    <Rout product={product} setProduct={setProduct} detail={detail} view={view} close={close} setClose={setClose} cart={cart} setCart={setCart}/>
+    <Footer />
+    </BrowserRouter>
     </>
   )
 }
